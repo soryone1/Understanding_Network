903ffb28dfd29d981a4d47fae9eef709f757df67
Jasper Wang <18850544459@163.com>
2019-11-24 14:02:44 -0500

    done with the client part

---
+++ Seamless_MTA_Button_LED/config.h
@@ -0,0 +1,6 @@
+//#define SECRET_SSID "王睿的 iPhone 8"
+//#define SECRET_PASS "Sorry1029"
+#define SECRET_SSID "DG1670A72"
+#define SECRET_PASS "DG1670AD3E272"
+//#define SECRET_SSID "sandbox370"
+//#define SECRET_PASS "ts0ats03!2gether?"
